881d5fca23c01d67ac9f829ab3276e62b0ac9f30
Insori <s2110@e-mirim.hs.kr>
2023-05-25 14:59:26 +0900

    메모라이제이션이 반영된 재귀 실행

---
+++ RecursionPrac/FibonacciAdv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecursionPrac
+{
+    internal class FibonacciAdv
+    {
+        private Dictionary<int, long> memo = new Dictionary<int, long>();
+
+        internal long Get(int n)
+        {
+            Console.WriteLine(n + " ");
+            if(n<0) { return 0; }
+            if(n==1) { return 1; }
+
+            if(memo.ContainsKey(n)) { return memo[n]; }
+            else
+            {
+                long value = Get(n - 2) + Get(n - 1);
+                memo[n] = value;
+                return value;
+            }
+            
+        }
+    }
+}
